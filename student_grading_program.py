e48927b2e381878e17531207266edaf47dc92716
Connergifford <111781584+Connergifford@users.noreply.github.com>
2022-11-28 12:25:07 -0500

    Add files via upload

---
+++ student_grading_program.py
@@ -0,0 +1,20 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+if student_scores == 91 - 100:
+    print('Outstanding')
+
+if student_scores == 81 - 90:
+    print('Exceeds Expectations')
+
+if student_scores == 71 - 80:
+    print('Acceptable')
+
+if student_scores == 0 - 70:
+    print('Fail')
+
+print(student_scores)
